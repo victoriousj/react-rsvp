2a376546b086bfbb8f5228cc2cc57a064640a8b4
Vic <victordjohnson@gmail.com>
2018-09-27 20:56:01 -0500

    added a funciton that will prevent the form from submitting but will still allow the creation of guests

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,29 +1,40 @@
 import React from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Header = props => 
-    <header>
-        <h1>RSVP</h1>
-        <p>A Treehouse App</p>
-        <form>
-            <input 
-                type="text" 
-                value={props.pendingGuest} 
-                onChange={props.changePendingGuest} 
-                placeholder="Invite Someone"
-            />
-            <button 
-                type="button" 
-                name="submit"
-                onClick={props.createNewGuest} 
-            >Submit</button>
-        </form>
-    </header>
+class Header extends Component {
+    static propTypes = {
+        pendingGuest: PropTypes.string,
+        createNewGuest: PropTypes.func.isRequired,
+        changePendingGuest: PropTypes.func.isRequired,
+    };
 
-Header.propTypes = {
-    pendingGuest: PropTypes.string,
-    createNewGuest: PropTypes.func.isRequired,
-    changePendingGuest: PropTypes.func.isRequired,
+    render() {
+        let peventDefaultAndCreateGuest = e => {
+            e.preventDefault();
+            this.props.createNewGuest();
+        }
+
+        return (
+            <header>
+                <h1>RSVP</h1>
+                <p>A Treehouse App</p>
+                <form onSubmit={peventDefaultAndCreateGuest}>
+                    <input 
+                        type="text" 
+                        value={this.props.pendingGuest} 
+                        onChange={this.props.changePendingGuest} 
+                        placeholder="Invite Someone"
+                        />
+                    <button 
+                        type="button" 
+                        name="submit"
+                        onClick={this.props.createNewGuest} 
+                        >Submit</button>
+                </form>
+            </header>
+        )
+    }
 }
 
 export default Header;
