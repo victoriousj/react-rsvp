711ed9a123c0b6c03edd3ab9657ff324b8a7e9f3
Vic Johnson <vjohnson@imagetrend.com>
2018-09-28 13:57:57 -0500

    re-converted the Header component into a staeless componenet for the sake of simplicity after learning how to do multiple inline calls in an event listener

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,40 +1,29 @@
 import React from 'react';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Header extends Component {
-    static propTypes = {
-        pendingGuest: PropTypes.string,
-        createNewGuest: PropTypes.func.isRequired,
-        changePendingGuest: PropTypes.func.isRequired,
-    };
+const Header = props =>
+    <header>
+        <h1>RSVP</h1>
+        <p>A Treehouse App</p>
+        <form onSubmit={e => {e.preventDefault(); props.createNewGuest()}}>
+            <input 
+                type="text" 
+                value={props.pendingGuest} 
+                onChange={props.changePendingGuest} 
+                placeholder="Invite Someone"
+                />
+            <button 
+                type="button" 
+                name="submit"
+                onClick={props.createNewGuest} 
+                >Submit</button>
+        </form>
+    </header>
 
-    render() {
-        let peventDefaultAndCreateGuest = e => {
-            e.preventDefault();
-            this.props.createNewGuest();
-        }
-
-        return (
-            <header>
-                <h1>RSVP</h1>
-                <p>A Treehouse App</p>
-                <form onSubmit={peventDefaultAndCreateGuest}>
-                    <input 
-                        type="text" 
-                        value={this.props.pendingGuest} 
-                        onChange={this.props.changePendingGuest} 
-                        placeholder="Invite Someone"
-                        />
-                    <button 
-                        type="button" 
-                        name="submit"
-                        onClick={this.props.createNewGuest} 
-                        >Submit</button>
-                </form>
-            </header>
-        )
-    }
-}
+Header.PropTypes = {
+    pendingGuest: PropTypes.string,
+    createNewGuest: PropTypes.func.isRequired,
+    changePendingGuest: PropTypes.func.isRequired,
+};
 
 export default Header;
